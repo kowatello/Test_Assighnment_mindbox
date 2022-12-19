f092b9292fb40f8d5147653661d7ceea545e9bf2
kowatello <120988040+kowatello@users.noreply.github.com>
2022-12-19 12:30:08 +0300

    some

---
+++ Test_Assighnment_mindbox/Program.cs
@@ -0,0 +1,16 @@
+﻿using Figure_areas;
+
+namespace Test_Assighnment_mindbox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var triangle = new Triangle(5, 7, 9);
+            Console.WriteLine("{0}, {1}", triangle.Area(), triangle.IsRight());
+
+            var circle = new Circle(1);
+            Console.WriteLine("{0}", circle.Area());
+        }
+    }
+}
